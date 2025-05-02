45afc269e1453ec6f363a3b810571524588128bc
Jordan <39-12602@sae.edu.za>
2025-05-02 22:09:13 +0200

    pwa

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,18 @@
-// next.config.js
-/** @type {import('next').NextConfig} */
-const withPWA = require('next-pwa')({
-    dest: 'public',
-    register: true,
-    skipWaiting: true,
-  });
-  
-  const nextConfig = {
-    // You can add other Next.js config here
-    reactStrictMode: true,
-  };
-  
-  module.exports = withPWA(nextConfig);
-  
+// next.config.mjs
+import nextPWA from 'next-pwa';
+import { join } from 'path';
+
+const withPWA = nextPWA({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === 'development',
+  buildExcludes: [/middleware-manifest\.json$/],
+});
+
+const nextConfig = {
+  reactStrictMode: true,
+  // Add any other Next.js config here
+};
+
+export default withPWA(nextConfig);
