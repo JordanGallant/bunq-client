12e4ba3076b455331fc6b3fb0a64673293d1db5b
Cao Minh Nguyen Cao Minh <c.m.nguyen-1@student.tudelft.nl>
2025-05-03 00:13:36 +0200

    Menu bar

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,12 @@
 'use client'
 import AvatarCameraButton from "./components/camera";
+import BottomMenuBar from "./components/menubar";
 export default function Home() {
   return (
     <>
     <h1>Hello</h1>
     <AvatarCameraButton/>
+    <BottomMenuBar />
     </>  
     );
 }
