4759d3e19ec54907bb4177ae13a277498f0a3819
Jordan <39-12602@sae.edu.za>
2025-05-03 03:59:12 +0200

    fix ui

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,9 @@
 'use client'
 import BankingMockup from "./banking/page";
-import BottomMenuBar from "../components/menubar";
 export default function Home() {
   return (
     <>
-    <h1>Hello</h1>
     <BankingMockup/>
-    <BottomMenuBar />
     </>  
     );
 }
