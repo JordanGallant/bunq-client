9df99b80a66d5cde2e83ce443cf871ff57d71764
Cao Minh Nguyen Cao Minh <c.m.nguyen-1@student.tudelft.nl>
2025-05-03 06:22:45 +0200

    Route payment button

--- src/components/home/ActionButton.js
+++ src/components/home/ActionButton.js
@@ -1,11 +1,12 @@
 'use client';
 
-export default function ActionButton({ text, icon: Icon, color }) {
+export default function ActionButton({ text, icon: Icon, color, onClick}) {
   
 
   return (
     <div className="flex flex-col items-center space-y-1">
       <button
+        onClick={onClick} 
         className={`${color} rounded-full w-14 h-14 flex items-center justify-center text-white hover:opacity-90 transition-opacity`}
         aria-label={text}
       >
--- src/components/home/ActionButtons.js
+++ src/components/home/ActionButtons.js
@@ -2,11 +2,13 @@
 
 import ActionButton from './ActionButton';
 import { FaArrowUp, FaArrowDown, FaPlus } from 'react-icons/fa';
+import { useRouter } from 'next/navigation';
 
 export default function ActionButtons() {
+  const router = useRouter();
   return (
     <section className="flex justify-around items-center pt-4">
-      <ActionButton text="Pay" icon={FaArrowUp} color="bg-orange-800" />
+      <ActionButton text="Pay" icon={FaArrowUp} color="bg-orange-800" onClick={() => router.push('/payment')}/>
       <ActionButton text="Request" icon={FaArrowDown} color="bg-blue-600" />
       <ActionButton text="Add" icon={FaPlus} color="bg-purple-600" />
     </section>
