02a35ed084f6c91e1eed6425d5570af0c5a31145
Jordan <39-12602@sae.edu.za>
2025-05-03 04:19:25 +0200

    fix handle click

--- src/components/home/ActionButton.js
+++ src/components/home/ActionButton.js
@@ -6,7 +6,6 @@ export default function ActionButton({ text, icon: Icon, color }) {
   return (
     <div className="flex flex-col items-center space-y-1">
       <button
-        onClick={handleClick}
         className={`${color} rounded-full w-14 h-14 flex items-center justify-center text-white hover:opacity-90 transition-opacity`}
         aria-label={text}
       >
